fb4d92ce9802bb80449f3764cdd393d8d79130b9
Adarsh Sriram <as2566@cornell.edu>
2020-08-10 21:41:26 +0530

    hid DB URI

--- clubhouse_manager/app.py
+++ clubhouse_manager/app.py
@@ -3,12 +3,13 @@ from flask import Flask, request, jsonify, render_template
 from db import Member, Facility, db
 import json
 import psycopg2
+import config
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
 
 # app config
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://rnjchqtvccnzma:50f02f117dc856205960e1a4ae949c032752d428d802d26615938c3976cd8497@ec2-52-204-20-42.compute-1.amazonaws.com: 5432/dcsbpv4h35ss8v"
+app.config["SQLALCHEMY_DATABASE_URI"] = config.URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
